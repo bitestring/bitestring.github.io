0022204021dea8172d28cd4d67fce1ea9e78d341
bitestring <81476430+bitestring@users.noreply.github.com>
2024-01-27 09:16:51 +0530

    Flake and Stackage update

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "bitestring.github.io";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      hs = pkgs.haskell.packages."ghc946"; # need to match Stackage LTS version from stack.yaml resolver
+      hs = pkgs.haskell.packages."ghc963"; # need to match Stackage LTS version from stack.yaml resolver
       dependencies = with pkgs; [
         # system deps
         zlib
@@ -31,7 +31,7 @@
 
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = dependencies;
-        # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath dependencies;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath dependencies;
       };
     };
 }
